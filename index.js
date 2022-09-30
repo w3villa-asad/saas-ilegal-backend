63f173f84de96f8dda31e0cdfeb7d87d504c06c9
w3villa-asad <asad.akhtar@w3villa.com>
2022-09-30 08:32:57 +0530

    make it better

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ const app = require('./src/app');
 // const { port } = require('./src/config');
 // const {job} = require('./src/contracts/web3_functions/cron_job')
 
-const port =  process.env.port || 5000;
-// try {
-    
-//     job.start()
-// } catch (error) {
-//     console.log(error)
-// }
-app.listen(port, function(){
-    console.log(`Express is running on port  {port}`, this.address.port, app.settings.env);
-});
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
