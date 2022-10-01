d1743d7635f9277e9ac5225ee761cf9f0c0b0219
w3villa-asad <asad.akhtar@w3villa.com>
2022-10-01 14:54:11 +0530

    your msg new

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = require('./src/app');
-const routes = require("../src/routes/api/createWallet");
 const port = process.env.PORT || 3000;
 // const { port } = require('./src/config');
 // const {job} = require('./src/contracts/web3_functions/cron_job')
@@ -8,8 +7,6 @@ app.get("/", (req, res)=>{
   res.send("Hey There");
 });
 app.set('port', port);
-
-app.use("/", routes);
 
 app.listen(port, ()=>{
     console.log("Express server listening on port %d in %s mode");
