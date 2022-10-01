45ce29e09b07381d026c6a029a44b1dac2a62460
w3villa-asad <asad.akhtar@w3villa.com>
2022-10-01 14:43:23 +0530

    your msg

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+const express = require("express");
 const app = require('./src/app');
+const port = process.env.PORT || 3000;
 // const { port } = require('./src/config');
 // const {job} = require('./src/contracts/web3_functions/cron_job')
+app.get("/", (req, res)=>{
+  res.send("Hey There");
+});
+app.set('port', port);
 
-app.listen(process.env.port || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.listen(port, ()=>{
+    console.log("Express server listening on port %d in %s mode");
   });
--- src/app.js
+++ src/app.js
@@ -11,11 +11,11 @@ const cors = require("cors");
 // const fileUpload = require("express-fileupload");
 
 const app = express();
+app.use("/", routes);
 app.use(cors({ origin: "*" }));
 app.use(bodyParser.urlencoded({ extended: "true" })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: "application/vnd.api+json" })); // parse application/vnd.api+json as json
-app.use("/", routes);
 
 
 module.exports = app;
